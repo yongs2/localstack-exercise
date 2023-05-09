2b5307c506642d98eb3a71faf0d111051b9d5444
yongs2 <yongs2@hotmail.com>
2023-05-09 14:44:59 +0900

    fix AWS_ENDPOINT_URL

--- learn-awslocal-dynamodb-golang/README.md
+++ learn-awslocal-dynamodb-golang/README.md
@@ -15,7 +15,7 @@ apk add go
 export AWS_ACCESS_KEY_ID="test"
 export AWS_SECRET_ACCESS_KEY="test"
 export AWS_DEFAULT_REGION="us-east-1"
-export LOCALSTACK_ENDPOINT="http://localhost:4566"
+export AWS_ENDPOINT_URL="http://localhost:4566"
 
 make run
 ```
--- learn-awslocal-dynamodb-golang/main.go
+++ learn-awslocal-dynamodb-golang/main.go
@@ -21,7 +21,7 @@ type Item struct {
 // export AWS_ACCESS_KEY_ID="test"
 // export AWS_SECRET_ACCESS_KEY="test"
 // export AWS_DEFAULT_REGION="us-east-1"
-// export LOCALSTACK_ENDPOINT="http://localhost:4566"
+// export AWS_ENDPOINT_URL="http://localhost:4566"
 func main() {
 	svc := localstack_DynamoDB()
 	createTable(svc)
@@ -34,7 +34,7 @@ func localstack_DynamoDB() *dynamodb.DynamoDB {
 	sess, _ := session.NewSession(&aws.Config{
 		Region:      aws.String(os.Getenv("AWS_DEFAULT_REGION")),
 		Credentials: credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), ""),
-		Endpoint:    aws.String(os.Getenv("LOCALSTACK_ENDPOINT")),
+		Endpoint:    aws.String(os.Getenv("AWS_ENDPOINT_URL")),
 	})
 
 	svc := dynamodb.New(sess)
